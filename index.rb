cdf32fc6ec098b1a5bf17a1427eb1f69f9974f62
Jason Marcell <jason@Jason-Marcells-MacBook.local>
2010-05-23 04:24:39 -0400

    doing work on the ajax branch

--- index.rb
+++ index.rb
@@ -1,11 +1,41 @@
 require 'rubygems'
+require 'dm-core'
 require 'sinatra'
 require 'haml'
 require 'sass'
 require 'pp'
 
+class Article
+  include DataMapper::Resource
+  property :id, Serial
+  property :title, Text
+  property :text, Text
+end
+
+DataMapper::Logger.new($stdout, :debug)
+DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/foo_database")
+DataMapper.auto_upgrade!
+
 get '/' do
-  haml :hello
+  @articles = Article.all
+  @articles = [] if @articles.nil?
+  haml :main
+end
+
+get '/ajax/articles' do
+  @articles = Article.all
+  @articles = [] if @articles.nil?
+  @articles.inject("") {|a,b| a + "<li><a href='/article/#{b.id}'>#{b.title}</a></li>"}
+end
+
+get '/article/:id' do
+  @article = Article.first :id => params[:id]
+  haml :article
+end
+
+post '/save' do
+  article = Article.create(:title => params[:title], :text => params[:text])
+  "<li><a href='/article/#{article.id}'>#{article.title}</a></li>"
 end
 
 get '/stylesheet.css' do
@@ -21,30 +51,44 @@ __END__
 %html
   %head
     %link{:rel => 'stylesheet', :href => '/stylesheet.css', :type => 'text/css'}
-    %link{:rel => 'stylesheet', :href => 'jquery-ui-1.8.1.custom/development-bundle/themes/le-frog/jquery.ui.core.css', :type => 'text/css'}
-    %link{:rel => 'stylesheet', :href => 'jquery-ui-1.8.1.custom/development-bundle/themes/le-frog/jquery.ui.theme.css', :type => 'text/css'}
-    %link{:rel => 'stylesheet', :href => 'jquery-ui-1.8.1.custom/development-bundle/themes/le-frog/jquery.ui.progressbar.css', :type => 'text/css'}
+    %link{:rel => 'stylesheet', :href => '/jquery-ui-1.8.1.custom/development-bundle/themes/le-frog/jquery.ui.core.css', :type => 'text/css'}
+    %link{:rel => 'stylesheet', :href => '/jquery-ui-1.8.1.custom/development-bundle/themes/le-frog/jquery.ui.theme.css', :type => 'text/css'}
+    %link{:rel => 'stylesheet', :href => '/jquery-ui-1.8.1.custom/development-bundle/themes/le-frog/jquery.ui.progressbar.css', :type => 'text/css'}
     %meta{'http-equiv' => "Content-Type", :content => "text/html; charset=utf-8"}
-    %script{:src => 'jquery-ui-1.8.1.custom/development-bundle/jquery-1.4.2.js', :type => 'text/javascript'}
-    %script{:src => 'jquery-ui-1.8.1.custom/development-bundle/ui/jquery.ui.core.js', :type => 'text/javascript'}
-    %script{:src => 'jquery-ui-1.8.1.custom/development-bundle/ui/jquery.ui.widget.js', :type => 'text/javascript'}
-    %script{:src => 'jquery-ui-1.8.1.custom/development-bundle/ui/jquery-ui-1.8.1.custom.js', :type => 'text/javascript'}
-    %script{:src => 'main.js', :type => 'text/javascript'}
+    %script{:src => '/jquery-ui-1.8.1.custom/development-bundle/jquery-1.4.2.js', :type => 'text/javascript'}
+    %script{:src => '/jquery-ui-1.8.1.custom/development-bundle/ui/jquery.ui.core.js', :type => 'text/javascript'}
+    %script{:src => '/jquery-ui-1.8.1.custom/development-bundle/ui/jquery.ui.widget.js', :type => 'text/javascript'}
+    %script{:src => '/jquery-ui-1.8.1.custom/development-bundle/ui/jquery-ui-1.8.1.custom.js', :type => 'text/javascript'}
+    %script{:src => '/jquery.jeditable.js', :type => 'text/javascript'}
+    %script{:src => '/jquery.form.js', :type => 'text/javascript'}
+    %script{:src => '/main.js', :type => 'text/javascript'}
     %script $(document).ready(main);
   %body
-    #container= yield
+    #container
+      %div= yield
     #footer
       Messing around with web technologies. Source
       %a{:href => 'http://github.com/jasmarc/rusty-scarecrow'} here
       \.
 
-@@hello
-%h1 rusty scarecrow
-#myform
-  %input{:type => 'textbox'}
-  %img{:src => 'add.png'} 
-%ul{:id => 'sortable'}
-%div{:id => 'progressbar'}
+@@main
+%h1 main page
+%form{:id => 'myform', :action => '/save', :method => 'post'}
+  %input{:type => 'text', :name => 'title'}
+  %br
+  %textarea{:name => 'text', :cols => '50', :rows => '5'}
+  %br
+  %input{:type => 'submit'}
+%ul{:id => 'output'}
+  - @articles.each do |article|
+    %li
+      %a{:href => "/article/#{article.id}"}= article.title
+
+@@article
+%h1= @article.title
+%div= @article.text
+%br
+%a{:href => '/'} back to main
 
 @@stylesheet
 body
--- public/main.js
+++ public/main.js
@@ -1,42 +1,28 @@
 function main() {
-  var recalculate_progress = function() {
-    var checked_boxes = $("input:checked").length
-    var all_boxes = $("input[type='checkbox']").length
-    var progress = 1
-    if(all_boxes > 0) {
-      progress = (100*checked_boxes/all_boxes) + 1
-    }
-    $("#progressbar").progressbar({
-      value: progress
-    });
-  }
-  
-  recalculate_progress();
-  
-  $("#myform").submit(function() {
-    $("ul").append("<li><input type='checkbox' />" + $("input[type='textbox']").val() + "</li>");
-    $("input[type='textbox']").val("");
-    $("input[type='checkbox']").click(function()
-    {
-        if($(this).is(":checked")){
-          $(this).parent().css("text-decoration", "line-through");
-        } else {
-          $(this).parent().css("text-decoration", "");
-        }
-        recalculate_progress();
-    });
-    recalculate_progress();
-  });
+  window.setInterval(function() {
+      $("ul").load("/ajax/articles");
+  }, 1000);
 
-  $(document).keyup(function(event) {
-    if (event.keyCode == 13) {
-      $("#myform").submit();
-    }
-  });
-  
-  $("img").click(function() {
-    $("#myform").submit();
-  });
-  
-  $("#sortable").sortable();
+  var options = {
+      //target: '#output',
+      // target element(s) to be updated with server response
+      //beforeSubmit: showRequest,
+      // pre-submit callback
+      success: function(responseText, statusText, xhr, $form) {
+          $("ul").append(responseText);
+      },
+      // post-submit callback
+      // other available options:
+      //url:       url         // override for form's 'action' attribute
+      //type:      type        // 'get' or 'post', override for form's 'method' attribute
+      //dataType:  null        // 'xml', 'script', or 'json' (expected server response type)
+      clearForm: true
+      // clear all form fields after successful submit
+      //resetForm: true        // reset the form after successful submit
+      // $.ajax options can be used here too, for example:
+      //timeout:   3000
+  };
+
+  // bind form using 'ajaxForm'
+  $('#myform').ajaxForm(options);
 }
